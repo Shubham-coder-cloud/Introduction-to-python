f0d985f6ad0c4c452f278232ae5139a90aa365c4
Shubham-coder-cloud <73992220+Shubham-coder-cloud@users.noreply.github.com>
2021-10-17 15:49:53 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,15 @@
+print(" Enter your name ")
+
+name = input()
+
+print("you are a piece of shit "+ name)
+
+import time
+
+time.sleep(4)
+
+print("okay you are not shit")
+
+time.sleep(3)
+
+print("You are a dipshit")
